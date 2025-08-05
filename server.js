68333da2a14ba58105a8aa95874fc00df198cccd
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-08-05 10:52:28 +0530

    added more env

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.NODE_ENV === "production" ? process.env.CLIENT_URL : 'http://localhost:5173',
     credentials: true,
   })
 );
