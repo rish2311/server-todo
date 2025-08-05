cd6a84707047416c44b497e891593060488e100e
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-08-05 10:16:47 +0530

    added vercel.json file

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
 // server.js
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoutes.js';
-import todoRoutes from './routes/todoRoutes.js';
-import { errorHandler } from './middleware/errorMiddleware.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import todoRoutes from "./routes/todoRoutes.js";
+import { errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Connect DB
 connectDB();
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/todos', todoRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/todos", todoRoutes);
 
 // Error handling
 app.use(errorHandler);
+
+app.listen("/", (req, res) => {
+  res.send("Server is running");
+});
 
 const PORT = process.env.PORT || 5000;
 
